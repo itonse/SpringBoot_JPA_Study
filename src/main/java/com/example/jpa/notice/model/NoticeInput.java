17a2857130d258a6a3f2d18076a94bef328ee4e1
itonse <govl3dnjs@gmail.com>
2023-05-24 22:20:57 +0900

    ch1.6 게시글 추가기능 (Q26 ~ Q30)

--- src/main/java/com/example/jpa/notice/model/NoticeInput.java
+++ src/main/java/com/example/jpa/notice/model/NoticeInput.java
@@ -5,12 +5,20 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
 public class NoticeInput {  // 입력받는 값
     // ID 와 등록일은 입력받을 필요가 없으므로, 따로 클래스를 만듦.
+    @Size(min = 10, max = 100, message = "제목은 10-100자 사이의 값입니다.")
+    @NotBlank(message = "제목은 필수 항목입니다.")
     private String title;
+
+    @Size(min = 50, max = 1000, message = "내용응 50-1000자 사이의 값입니다.")
+    @NotBlank(message = "내용은 필수 항목입니다.")
     private String contents;
 }
--- src/test/java/com/example/jpa/Sample1ApplicationTests.java
+++ src/test/java/com/example/jpa/Sample1ApplicationTests.java
@@ -1,8 +1,10 @@
 package com.example.jpa;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SpringBootTest
 class Sample1ApplicationTests {
 
